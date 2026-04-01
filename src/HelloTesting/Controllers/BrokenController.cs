35e76bb65bbb8802d7f655889cd95b9dfb55c844
Karim <71331746+kurious99@users.noreply.github.com>
2026-04-01 11:57:30 +0700

    test: add broken controller (#24)
    * test: add broken controller
    
    * update
    
    ---------
    
    Co-authored-by: karim <karim@gmail.com>

---
+++ src/HelloTesting/Controllers/BrokenController.cs
@@ -0,0 +1,14 @@
+﻿namespace HelloTesting.Controllers;
+
+using Microsoft.AspNetCore.Mvc;
+
+[ApiController]
+[Route("api/[controller]")]
+public sealed class BrokenController : ControllerBase
+{
+    [HttpGet]
+    public IActionResult Get()
+    {
+        return Ok(new { message = "fixed" });
+    }
+}
